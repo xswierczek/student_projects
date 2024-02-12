42b75d90af66083841c041220cc56a82ef98abf7
xswierczek <max.swierczek@gmail.com>
2024-02-12 10:13:15 +0000

    studentmachine send

---
+++ cpp/szablon/kompiluj.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+echo "----Kompilacja programu----"
+g++ -o main main.cpp
+echo "---Kompilacja ukonczona----"
+echo "---Uruchamianie programu---"
+chmod +x main
+./main
+echo "---Zakonczono program---"
