b764265dee391b3ec5f482505ead2073da70ea89
xswierczek <max.swierczek@gmail.com>
2024-09-23 11:04:59 +0000

    studentmachine send

---
+++ cpp/cwiczenie1/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <algorithm>
+	using namespace std;
+
+	int main(){
+	int liczby[4] =  {3,2,5,4}
+
+		cout<<"Posortowane liczby"<<;
+
+
+		for(int x = 0; x < 4; x++) {
+		}
+		cout <<  endl;z	return 0;
+		}
+
+	
+	
